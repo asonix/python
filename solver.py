11b520cf37a4aa76cd340dacf91946f835544a58
asonix <asonix.dev@gmail.com>
2014-01-28 22:43:55 -0600

    solver was created to help me with an application for an apprenticeship over the summer and list was created to help solve a puzzle with a high reward in my dad's company

---
+++ solver.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import math
+u = 0.125
+for i in xrange(0, 25):
+	u = math.sin(math.pi*u)
+	print "iteration: {:n} - value: {:n}".format((i+1),u)
+
+p = 0.125
+for i in xrange(0, 25):
+	p = math.sin(math.pi*p)
+	if i == 3:
+        	p *= 100000
+		print p
+        	p = math.floor(p)
+		print p
+        	p /= 100000
+	print "iteration: {:n} - value: {:n}".format((i+1),p)
+print "Final unaltered value: {:n}".format(u)
+print "Final altered value: {:n}".format(p)
+
